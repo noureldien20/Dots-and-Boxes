7002397b465976b6dd6ec6a1ce9805b8cf741163
noureldien <noureldientaha@gmail.com>
2023-12-24 14:25:57 +0200

    back to old input fn

--- temperory game.c
+++ temperory game.c
@@ -142,12 +142,10 @@ void print_boxes_color(short int c,short int i)
     if (boxes[c-1][i] == '1')
     {
         printf(back_cyan"       ");
-        current_game.player_1.score++;
     }
     else if(boxes[c-1][i] == '2')
     {
         printf(back_green"       ");
-        current_game.player_2.score++;
     }
     else
     {
@@ -229,15 +227,6 @@ char big(char c)
         return c ;
     }
 }
-
-/*void time_passed(){
-
-   while(win != n*n){
-      sleep(1) ;
-      game.time++ ;
-   }
-   
-}*/
 
 void check_edges()
 {
@@ -509,6 +498,8 @@ void redo(Stack *undo_stack,Stack *redo_stack, game *current)
 
 void switch_turn()
 {
+    int temp = number_of_filled_boxes();
+
     if (turn == '1') 
     {
         current_game.player_1.number_of_moves++;
@@ -518,13 +509,24 @@ void switch_turn()
         current_game.player_2.number_of_moves++;
     }
 
-    if(number_of_filled_boxes() == current_game.previous_sum)
+    if(temp == current_game.previous_sum)
     {
         empty_both_stacks();
         turn = (turn == '1') ? '2' : '1';
         current_game.turn = turn;
     }
-    current_game.number_of_remaining_boxes =  (current_game.size * current_game.size) - number_of_filled_boxes();
+    else
+    {
+        if (turn == '1') 
+        {
+            current_game.player_1.score = temp - current_game.previous_sum;
+        } 
+        else 
+        {
+            current_game.player_2.score = temp - current_game.previous_sum;
+        }
+    }
+    current_game.number_of_remaining_boxes =  (current_game.size * current_game.size) - temp;
 }
 
 unsigned short int check_node(char x)
@@ -539,30 +541,6 @@ unsigned short int check_node(char x)
         return 0;
     }
 }
-
-/*short int absolute(short int x)
-{
-    if(x<0)
-    {
-        return -1 * x;
-    }
-    else
-    {
-        return x;
-    }
-}
-
-unsigned short int min(unsigned short int x,unsigned short int y)
-{
-    if(x<y)
-    {
-        return x ;
-    }
-    else
-    {
-        return y ;
-    }
-}*/
 
 int min(int a, int b) 
 {
@@ -582,7 +560,7 @@ void input_nodes()
 
         if(!(check_node(temp[0]) && check_node(temp[1]) && check_node(temp[2]) && check_node(temp[3])))
         {
-            printf("Invalid input\n");
+            printf("Invalid input1\n");
             clearInputBuffer();
             input_nodes() ;
         }
@@ -594,16 +572,18 @@ void input_nodes()
             c1 = (unsigned short int)temp[2] - '0' ; 
             c2 = (unsigned short int)temp[3] - '0' ;
 
-            if( !((r1 == r2 || c1 == c2) && ((abs(r1 - r2) == 1) || (abs(c1 - c2) == 1))))
+            if(!
+                (r1==r2 || c1==c2) &&   //nodes are adjacent
+                (abs(r1-r2)==1 || abs(c1-c2==1)) //short line not long line
+                )
             {
-                printf("Invalid input\n");
-                clearInputBuffer();
+                printf("Invalid input2\n") ;
                 input_nodes() ;
             }
-            else if(row_edges[r1 - 1][min(c1,c2) - 1] != '\0' && (r1 == r2) || col_edges[min(r1,r2)-1][c1 - 1] != '\0' && (c1 == c2))
-            {
-                printf("Invalid input\n");
-                clearInputBuffer();
+            else if(row_edges[r1-1][min(c1,c2)-1]!='\0' && r1==r2 ||
+                col_edges[min(r1,r2)-1][c1-1]!='\0' && c1==c2)
+            {   
+                printf("Invalid input3\n") ;
                 input_nodes() ;
             }
             /*else
@@ -620,7 +600,7 @@ void input_nodes()
         }
         else
         {
-            printf("Invalid input\n");
+            printf("Invalid input4\n");
             clearInputBuffer();
             input_nodes();
         }
