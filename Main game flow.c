eba97094b717d5da81b5ce7f7a5e93eb7dc3dcf1
Ahmed-Ragy-3 <ahmedragy2005@gmail.com>
2023-12-26 13:43:21 +0200

    mt8yr4 7aga delo2ty fy el file (Main game flow)

--- Main game flow.c
+++ Main game flow.c
@@ -12,27 +12,34 @@
 /******************************** بسم الله الرحمن الرحيم  *********************************/
 
 int main(){
+printf("Welcome to Dots & Boxes game\n");
+
+while(1 /* lw m3ml4 quit */){ // bta3t el brnamg kolo
+
+print_menu();
 
 pthread_t time_thread ;
 pthread_create(&time_thread, NULL, time_passed, NULL) ;
 
-    printf("Welcome to Dots & Boxes game\n");
+print_grid();
 
-    while(1)
-    {
-        print_menu();
-        print_grid();
+while(1 /* number of filled boxes = n*n */){
 
-       // while(number_of_filled_boxes() != n*n)
-        {
-            //current_game.previous_sum = number_of_filled_boxes();
-            input_nodes();
-            check_edges();
-            print_grid();
-            printf("%d",t);
-            //switch_turn();
-            //display_stats();
-        }
-    }
+    //current_game.previous_sum = number_of_filled_boxes();
+    input_nodes();
+    check_edges();
+    print_grid();
+    printf("%d",t);
+    //switch_turn();
+    //display_stats();
+}
+//free game matrices & variables
+free(row_edges) ;
+free(col_edges) ;
+free(boxes) ;
+free(dfs);
+t = 0 ;
+
 }
 
+}
